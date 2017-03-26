c98e68658641ce06cf00113a7faa4d14010ccc75
Jasonlee5083 <zoo0530@gmail.com>
2017-03-25 22:38:48 -0600

    mar 25

--- excercises/angularpractice/Angular_TodWebsite/app.js
+++ excercises/angularpractice/Angular_TodWebsite/app.js
@@ -9,22 +9,22 @@ app.controller('mainController',['$scope','$http', function($scope,$http)
 	});
 	
 	$scope.submit = function(info){
-
-	
 	$http.post('http://api.vschool.io/jason/todo/',info).then(function(response){
+		$scope.postMe.push(response.data);
 	console.log("success")
 })
 	}
+	
 $scope.remove = function(index,id){
 	
-	$http.delete('http://api.vschool.io/jason/todo/',+ $scope.remove[index].id).then(function(){
-		$scope.remove.splice(index,1);
+	$http.delete('http://api.vschool.io/jason/todo/',+ $scope.remove[index]._id).then(function(){
+		$scope.remove.splice(index,1) = response;
 	})
 }
 
-//$scope.edit = function(index) {
-//	
-//}
+$scope.edit = function(index) {
+	
+}
 
 }]);
 
@@ -34,7 +34,12 @@ $scope.remove = function(index,id){
 //	msg: 'hello world'
 //}
 	
-
+//$scope.remove = function(index,id){
+//	
+//	$http.delete('http://api.vschool.io/jason/todo/',+ $scope.remove[index].id).then(function(){
+//		$scope.remove.splice(index,1);
+//	})
+//}
 
 
 
---
+++ excercises/formexample/Travelform/app.js
@@ -0,0 +1,24 @@
+
+document.getElementById("button").addEventListener("click", function() {
+	var form = document.getElementById("form");
+	var fname = form.fname.value;
+	var lname = form.lname.value;
+	var age = form.age.value;
+	var gender = form.gender.value;
+	var location = form.location.value;
+	var restrictions = form.restrictions;
+
+	for (var i=0; i<form.restrictions.length; i++) 
+	{
+		if(form.restrictions[i].checked)
+			{
+				restrictions.push(form.restrictions[i].value);
+			}
+	}
+var formOutput = `The name is ${fname} ${lname}, and the age is ${age}, and the gender is ${gender}, and the location is ${location}, and the Dietary restrictions is ${restrictions}`
+	
+	
+	alert(formOutput);
+
+												   }
+												  )
